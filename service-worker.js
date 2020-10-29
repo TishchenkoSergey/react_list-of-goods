7dfa914cb88c044ae53c532f8aa3ebeddee1ea20
TishchenkoSergey <tishchenko.seriy@gmail.com>
2020-10-29 23:38:41 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.7d52fdaa7f5f09bfa9b24f3137220291.js"
 );
 
 self.addEventListener('message', (event) => {
